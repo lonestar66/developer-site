aa8f8af4161e5ff9fcf7b1fd46f13f546dde036c
lonestar66 <cacooke@gmail.com>
2018-05-30 21:56:57 -0400

    Initial Matchup
    Synching all servers

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+	echo '<p>Hello World</p>';
+?>
+Soon to be the new site of Joe's Jubilant Jamboree.
+</body>
+</html>
